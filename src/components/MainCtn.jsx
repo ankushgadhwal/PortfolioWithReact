f34c5f53510923bc50ccbab8a6d639c70c11fcb5
ankush <ankushgadhwal999@gmail.com>
2024-10-23 15:05:03 +0530

    favicon updated

--- src/components/MainCtn.jsx
+++ src/components/MainCtn.jsx
@@ -1,36 +1,35 @@
 import { Children } from "react";
-import { Link } from "react-router-dom";
-import "./MainCtn.css"
+import { Link, useLocation } from "react-router-dom";
+import "./MainCtn.css";
 
 // eslint-disable-next-line react/prop-types
 const MainCtn = ({ children }) => {
+  let { pathname } = useLocation();
+  const pageLinks = [
+    { name: "About", link: "/" },
+    { name: "Resume", link: "/resume" },
+    { name: "Portfolio", link: "/portfolio" },
+    { name: "Contact", link: "/contact" },
+  ];
+
   return (
     <>
       <div className="col-xl-9 col-lg-12">
         <div className="box-outer">
           <div className="navbar">
             <ul className="nav">
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/resume" className="nav-link">
-                  Resume
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/portfolio" className="nav-link">
-                  Portfolio
-                </Link>
-              </li>
-
-              <li className="nav-item">
-                <Link to="/contact" className="nav-link">
-                  Contact
-                </Link>
-              </li>
+              {pageLinks.map((page, idx) => (
+                <li
+                  className={`nav-item${
+                    pathname === page.link ? " active" : ""
+                  }`}
+                  key={idx}
+                >
+                  <Link to={page.link} className="nav-link">
+                    {page.name}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
