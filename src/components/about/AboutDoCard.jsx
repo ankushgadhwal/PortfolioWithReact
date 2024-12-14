30322180ae7afd1b0599ca1d12c012939717bc1e
Ankush Gadhwal <112737184+ankushgadhwal@users.noreply.github.com>
2024-12-14 16:34:59 +0530

    Update AboutDoCard.jsx

--- src/components/about/AboutDoCard.jsx
+++ src/components/about/AboutDoCard.jsx
@@ -3,13 +3,15 @@ function AboutDoCard({ card }) {
   return (
     <>
       <div className="col-sm-12 col-lg-6">
-        <div className="item-inner d-flex">
-          <div className="inner-img me-3">
-            <img src={`./images/${card.img}.svg`} alt={card.title} />
-          </div>
-          <div className="inner-content">
-            <h3>{card.title}</h3>
-            <p>{card.description}</p>
+        <div className="card-wrapper">
+          <div className="item-inner d-flex">
+            <div className="inner-img me-3">
+              <img src={`./images/${card.img}.svg`} alt={card.title} />
+            </div>
+            <div className="inner-content">
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+            </div>
           </div>
         </div>
       </div>
